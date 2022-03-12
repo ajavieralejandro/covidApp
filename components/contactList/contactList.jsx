caf608841538012fe0c24f1c0c1f71f478998e65
Javier <amorosijavier@gmail.com>
2022-03-12 19:28:38 -0300

    contacts list added

--- components/contactList/contactList.jsx
+++ components/contactList/contactList.jsx
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import React,{useState,useEffect} from 'react'
-import { ScrollView,View, Text, FlatList } from 'react-native';
+import { ActivityIndicator } from 'react-native';
+import { ScrollView,View, Text, FlatList,StyleSheet,Image } from 'react-native';
 import UserAvatar from 'react-native-user-avatar';
 import SearchBar from '../searchBar/searchBar';
 
-const renderItem = ({item,index}) =>{
-    console.log(index)
+
+const renderItem = ({item}) =>{
     return(
-        <View key={index}>
-            <Text>{item.name.first}</Text>
-            <Text>{item.name.last}</Text>
+        <View style={styles.itemWrapperStyle}>
+            <Image style={styles.itemImageStyle} source={{uri : item.picture.large}} />
+            <View >
+            <Text>{item.name.title} {item.name.first} {item.name.last}</Text>
+            </View>
 
 
         </View>
@@ -18,22 +21,63 @@ const renderItem = ({item,index}) =>{
 }
 
 
+
+const styles = StyleSheet.create({
+    itemWrapperStyle : {
+        flexDirection : "row",
+        paddingHorizontal : 16,
+        paddingVertical : 16,
+        borderBottomWidth : 1,
+        borderColor : "#ddd",
+    },
+    itemImageStyle : {
+        width : 50,
+        height : 50,
+        marginRight : 16
+    },
+    loaderStyle:{
+        marginVertical : 16,
+        alignItems : 'center'
+    }
+    
+})
+
+
 export default function ContactList() {
    
     const [contacts, setContacts] = useState([]);
+    const [page,setPage] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+    const loadMoreItem = () =>{
+        setPage(page+1);
+    }
+
+    const renderLoader = () =>{
+        return(
+            isLoading? 
+            <View>
+                <ActivityIndicator size="large" color="black" />
+            </View>: null
+        )
+    }
+    
     const getContacts = () =>{
+        setIsLoading(true);
         
-        axios.get("https://randomuser.me/api/?page=3&results=100"
+        axios.get(`https://randomuser.me/api/?page=${page}&results=20`
             ).then(res=>{
-                setContacts(res.data.results);
-                console.log(res.data.results);
+                setContacts([...contacts,...res.data.results]);
+                setIsLoading(false);
+
+
             });
 
     }
 
     useEffect(()=>{
         getContacts()
-    },[]);
+    },[page]);
+
 
     return (
         <View style={{flex:1}}>
@@ -41,10 +85,13 @@ export default function ContactList() {
                         <SearchBar label="buscar contacto" />
                         </View>
 
-        <ScrollView >
-            <FlatList data={contacts} renderItem={renderItem} keyExtractor={item=>item.email}/>
+            <FlatList 
+                data={contacts} renderItem={renderItem} keyExtractor={item=>item.email}
+                ListFooterComponent={renderLoader}
+                onEndReached={loadMoreItem}
+                    
+            />
 
-        </ScrollView>
         </View>
 
     )
